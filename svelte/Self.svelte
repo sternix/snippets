3fac1200b414a659f324f70cbedb79c94051d98c
Yavuz Tanrıverdi <sternix@gmail.com>
2024-05-30 09:53:37 +0300

    svelte:self örneği eklendi

---
+++ svelte/Self.svelte
@@ -0,0 +1,38 @@
+// https://svelte.dev/docs/special-elements#svelte-self 
+
+Counter.svelte
+```svelte
+<script>
+    let { count } = $props();
+</script>
+
+{#if count > 0}
+    <p>counting down {count}</p>
+    <svelte:self count={count - 1} />
+{:else}
+    <p>Lift off</p>
+{/if}
+```
+
+App.svelte
+```svelte
+<script>
+    import Counter from "./Counter.svelte";
+</script>
+
+<Counter count="5" />
+```
+
+Sonuç:
+
+counting down 5
+
+counting down 4
+
+counting down 3
+
+counting down 2
+
+counting down 1
+
+Lift off
