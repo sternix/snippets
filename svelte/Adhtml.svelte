836756a0c286d363d6cea0ec5e76e9b4c93c08ce
Yavuz Tanrıverdi <sternix@gmail.com>
2024-05-30 15:42:18 +0300

    @html ve action için use:focus örneği eklendi

---
+++ svelte/Adhtml.svelte
@@ -0,0 +1,13 @@
+<script>
+    let markup = '<h1 style="color: red">Hello!</h1>';
+</script>
+
+<textarea bind:value={markup} rows={5}></textarea>
+{@html markup}
+
+<style>
+    textarea {
+        width: 95vw;
+    }
+</style>
+
---
+++ svelte/UseFocus.svelte
@@ -0,0 +1,9 @@
+<script>
+	// sayfa yüklendiğinde otomatik olarak focus oluyor
+    let name = "";
+    const focus = (element) => element.focus();
+</script>
+
+<input type="text" value="Klmn" />
+
+<input bind:value={name} use:focus />
