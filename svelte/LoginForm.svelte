66ccbf69d09fbfcb31008c31b38f5c9cc1e86d07
Yavuz Tanrıverdi <sternix@gmail.com>
2024-05-30 17:02:05 +0300

    Login Signup form örneği eklendi

---
+++ svelte/LoginForm.svelte
@@ -0,0 +1,62 @@
+<script>
+    let username = $state("");
+    let password = $state("");
+
+    function printFields() {
+        console.log(`Username: ${username} - Password: ${password}`);
+    }
+
+    function login(e) {
+        e.preventDefault();
+        console.log("You pressed Login.");
+        printFields();
+    }
+
+    function signup(e) {
+        e.preventDefault();
+        console.log("You pressed Signup.");
+        printFields();
+    }
+</script>
+
+<section>
+    <form onsubmit={login}>
+        <label>
+            Username
+            <input required bind:value={username} />
+        </label>
+        <label>
+            Password
+            <input type="password" required bind:value={password} />
+        </label>
+        <div class="buttons">
+            <button>Login</button>
+            <button type="button" onclick={signup}>Sign Up</button>
+        </div>
+    </form>
+</section>
+
+<style>
+    .buttons {
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.5rem;
+        margin-top: 1rem;
+    }
+
+    form {
+        display: inline-block;
+    }
+
+    input {
+        display: block;
+        margin-top: 0.3rem;
+    }
+
+    label {
+        display: block;
+        font-size: 1.5rem;
+        margin-top: 0.5rem;
+    }
+</style>
+
