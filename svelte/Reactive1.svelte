fcf7db16db9b6d202ec83c1b87f247bd7c254058
Yavuz Tanrıverdi <sternix@gmail.com>
2024-05-31 10:22:31 +0300

    bir örnek eklendi

---
+++ svelte/Reactive1.svelte
@@ -0,0 +1,11 @@
+<script>
+// güzel bir örnek
+    let numbers = $state([1, 2, 3]);
+</script>
+
+<button onclick={() => numbers.push(numbers.length + 1)}> push </button>
+
+<button onclick={() => numbers.pop()}> pop </button>
+
+<p>{numbers.join(" + ") || 0} = {numbers.reduce((a, b) => a + b, 0)}</p>
+
